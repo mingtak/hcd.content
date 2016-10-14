0b82801fb75884ce28c9bac02848b52a9607bb57
andy fang <andy@mingtak.com.tw>
2016-10-14 12:17:34 +0100

    add climate_listing_view for folder

---
+++ src/hcd/content/browser/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+
+class GetClimate(BrowserView):
+
+    template = ViewPageTemplateFile("template/get_climate.pt")
+
+    def __call__(self):
+        context = self.context
+        request = self.request
+
+        para = request.get('para')
+        if not para:
+            return '<div></div>'
+
+        self.brain = api.content.find(Type='Climate', event=para)
+        return self.template()
+
+
+class ClimateListingView(BrowserView):
+
+    template = ViewPageTemplateFile("template/climate_listing_view.pt")
+
+    def __call__(self):
+        return self.template()
+
+
+    def getEventList(self):
+        return api.portal.get_registry_record('hcd.content.browser.siteSetting.ISiteSetting.event')
