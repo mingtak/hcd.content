809b640c92231e37743f891febf831057e314421
TsungWei Hu <marr.tw@gmail.com>
2016-11-02 17:25:39 +0800

    Update Catalog Indexer

--- src/hcd/content/catalog.py
+++ src/hcd/content/catalog.py
@@ -6,26 +6,40 @@ from Products.CMFPlone.utils import safe_unicode
 from hcd.content.interfaces import IClimate
 
 
-@indexer(IClimate)
-def clrsYear_indexer(obj):
-    if obj.clrsby:
-        return int(obj.clrsby)
-
-
-@indexer(IClimate)
-def event_indexer(obj):
-    event = getattr(obj, 'hpng')
-
-#    import pdb; pdb.set_trace()
-
-    if event:
-        event = event.split(';')
+def hpng(obj):
+    hpng = getattr(obj, 'hpng')
+    if hpng:
+        hpng = hpng.split(';')
     else:
         return None
-
-    for i in range(len(event)):
-        if int(event[i]) < 1:
-            event[i] = event[i].strip()
+    for i in range(len(hpng)):
+        if int(hpng[i]) < 1:
+            hpng[i] = hpng[i].strip()
             continue
-        event[i] = event[i].strip()[0:4]
-    return event
+    return hpng
+
+
+@indexer(IClimate)
+def ctgr1_indexer(obj):
+    return [i[0:2] for i in hpng(obj)]
+
+
+@indexer(IClimate)
+def ctgr2_indexer(obj):
+    return [i[0:4] for i in hpng(obj)]
+
+
+@indexer(IClimate)
+def degree_indexer(obj):
+    return [i[7:8] for i in hpng(obj)]
+
+
+@indexer(IClimate)
+def lasting_indexer(obj):
+    return [i[8:9] for i in hpng(obj)]
+
+
+@indexer(IClimate)
+def clrsby_indexer(obj):
+    if obj.clrsby:
+        return int(obj.clrsby)
